b6d113300209d2f8694d4fc2eb46e4bc6dfd8103
vagrant <vagrant@localhost.localdomain>
2015-11-13 14:58:07 -0500

    some bug fixes

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/leonidlm/packer-builder-softlayer/builder/softlayer"
+	"github.com/zapman449/packer-builder-softlayer/builder/softlayer"
 	"github.com/mitchellh/packer/packer/plugin"
 )
 
